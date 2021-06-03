6af07170462d3d2222f782d362fd051056fb5316
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2021-06-03 18:28:52 -0300

    feat: added home page file

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:test/ui/pages/home/home_page.dart';
 
 class AppModule extends Module {
   @override
   final List<Bind> binds = [];
 
   @override
-  final List<ModularRoute> routes = [];
+  final List<ModularRoute> routes = [
+    ChildRoute('/', child: (_, args) => HomePage()),
+  ];
 }
