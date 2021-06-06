06eddb6006155112db003ad2b23353132e0fcf7d
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2021-06-05 22:58:12 -0300

    feat: implemented example layout

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:look_up/ui/components/app_logo_component.dart';
+
+import '../../components/app_logo_component.dart';
+import 'widgets/appbar_menu_item_widget.dart';
+import 'widgets/news_item_widget.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -14,56 +17,71 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        return Scaffold(
-          extendBodyBehindAppBar: true,
-          appBar: PreferredSize(
-            preferredSize: Size(constraints.maxWidth, 1000),
-            child: Container(
-              color: Colors.white.withOpacity(0.2),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    AppLogoComponent(size: 40),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text('SPACE X'),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text('NASA'),
-                        ),
-                      ],
-                    )
-                  ],
+        if (constraints.maxWidth >= 800) {
+          return Scaffold(
+            extendBodyBehindAppBar: true,
+            appBar: PreferredSize(
+              preferredSize: Size(constraints.maxWidth, 1000),
+              child: Container(
+                color: Colors.white.withOpacity(0.2),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const AppLogoComponent(size: 40),
+                      Row(
+                        children: [
+                          const AppbarMenuItemWidget(text: 'SPACE X'),
+                          const AppbarMenuItemWidget(text: 'NASA'),
+                          const AppbarMenuItemWidget(text: 'BLUEORIGIN'),
+                          const AppbarMenuItemWidget(text: 'VIRGIN GALACTIC'),
+                          const AppbarMenuItemWidget(text: 'ESA'),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
+            body: Center(
+              child: ListView(
+                padding: const EdgeInsets.only(),
+                children: [
+                  NewsItemWidget(constraints: constraints),
+                  NewsItemWidget(constraints: constraints),
+                  NewsItemWidget(constraints: constraints),
+                ],
+              ),
+            ),
+          );
+        }
+        return Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            elevation: 0.0,
+            backgroundColor: Colors.white.withOpacity(0.2),
+            title: const AppLogoComponent(),
+            actions: [
+              PopupMenuButton(itemBuilder: (context) {
+                return [
+                  const PopupMenuItem(child: Text('SPACE X')),
+                  const PopupMenuItem(child: Text('NASA')),
+                  const PopupMenuItem(child: Text('BLUEORIGIN')),
+                  const PopupMenuItem(child: Text('VIRGIN GALACTIC')),
+                  const PopupMenuItem(child: Text('ESA'))
+                ];
+              })
+            ],
           ),
           body: Center(
-            child: Container(
-              height: double.infinity,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    'lib/assets/test_image.jpg',
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: SafeArea(
-                child: Text(
-                  '',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 50,
-                  ),
-                ),
-              ),
+            child: ListView(
+              padding: const EdgeInsets.only(),
+              children: [
+                NewsItemWidget(constraints: constraints),
+                NewsItemWidget(constraints: constraints),
+                NewsItemWidget(constraints: constraints),
+              ],
             ),
           ),
         );
