79ca1ba2ce7df6ab47727466721ba662d56122dc
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2021-06-12 22:04:56 -0300

    feat: added news model

---
+++ lib/infra/models/news_model.dart
@@ -0,0 +1,64 @@
+import 'package:faker/faker.dart';
+
+import '../../domain/entities/news_entity.dart';
+
+class NewsModel extends NewsEntity {
+  NewsModel({
+    required int id,
+    required String title,
+    required String url,
+    required String imageUrl,
+    required String newsSite,
+    required String summary,
+    required DateTime publishedAt,
+    required DateTime updatedAt,
+    required bool featured,
+  }) : super(
+          id: id,
+          title: title,
+          url: url,
+          imageUrl: imageUrl,
+          newsSite: newsSite,
+          summary: summary,
+          publishedAt: publishedAt,
+          updatedAt: updatedAt,
+          featured: featured,
+        );
+
+  factory NewsModel.fromJson(Map<String, dynamic> json) {
+    return NewsModel(
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+      imageUrl: json['imageUrl'],
+      newsSite: json['newsSite'],
+      summary: json['summary'],
+      publishedAt: json['publishedAt'],
+      updatedAt: json['updatedAt'],
+      featured: json['featured'],
+    );
+  }
+
+  factory NewsModel.fake() {
+    final faker = Faker();
+
+    return NewsModel(
+      id: faker.randomGenerator.integer(999),
+      title: faker.lorem.sentence(),
+      url: faker.internet.httpsUrl(),
+      imageUrl: faker.image.image(),
+      newsSite: faker.lorem.word(),
+      summary: faker.lorem.sentence(),
+      publishedAt: faker.date.dateTime(),
+      updatedAt: faker.date.dateTime(),
+      featured: faker.randomGenerator.boolean(),
+    );
+  }
+
+  static List<NewsModel> fakeList(int length) {
+    return List<NewsModel>.generate(
+      length,
+      (index) => NewsModel.fake(),
+    );
+  }
+}
