faec22b8d09307e52423aa56838e17676277f341
Gabriel Bertollo <gabrielbertollo.gb@gmail.com>
2021-06-13 21:10:28 -0300

    feat: added news data to widget

--- lib/ui/pages/home/widgets/news_item_widget.dart
+++ lib/ui/pages/home/widgets/news_item_widget.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../domain/entities/news_entity.dart';
+
 class NewsItemWidget extends StatefulWidget {
+  final NewsEntity news;
   final BoxConstraints constraints;
 
   const NewsItemWidget({
+    required this.news,
     required this.constraints,
     Key? key,
   }) : super(key: key);
@@ -54,29 +58,34 @@ class _NewsItemWidgetState extends State<NewsItemWidget> {
         Positioned(
           top: widget.constraints.maxHeight / 1.3,
           left: widget.constraints.maxWidth >= 800 ? 120 : 0,
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 200),
-            color: Colors.white.withOpacity(cardOpacity),
-            child: Padding(
-              padding: widget.constraints.maxWidth >= 800
-                  ? const EdgeInsets.all(8.0)
-                  : const EdgeInsets.fromLTRB(20.0, 8.0, 8.0, 8.0),
-              child: AnimatedOpacity(
-                opacity: textOpacity,
-                duration: const Duration(milliseconds: 200),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('COMPANY'),
-                    const Text(
-                      'TITLE',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: widget.constraints.maxWidth / 1.5,
+            ),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              color: Colors.white.withOpacity(cardOpacity),
+              child: Padding(
+                padding: widget.constraints.maxWidth >= 800
+                    ? const EdgeInsets.all(8.0)
+                    : const EdgeInsets.fromLTRB(20.0, 8.0, 8.0, 8.0),
+                child: AnimatedOpacity(
+                  opacity: textOpacity,
+                  duration: const Duration(milliseconds: 200),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(widget.news.newsSite.toUpperCase()),
+                      Text(
+                        widget.news.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
-                    ),
-                    const Text('Example text'),
-                  ],
+                      Text(widget.news.summary),
+                    ],
+                  ),
                 ),
               ),
             ),
